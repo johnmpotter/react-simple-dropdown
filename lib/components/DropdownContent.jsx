3ca473db8bbcb64012b1becffc02c458f8d37f07
John Potter <johnpotter1440@gmail.com>
2017-01-18 19:05:00 -0800

    Create NPM ready files

--- lib/components/DropdownContent.jsx
+++ lib/components/DropdownContent.jsx
@@ -1,32 +1,46 @@
-import React, { createClass, PropTypes } from 'react';
 
-const DropdownContent = createClass({
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var DropdownContent = (0, _react.createClass)({
   displayName: 'DropdownContent',
 
   propTypes: {
-    children: PropTypes.node,
-    className: PropTypes.string
+    children: _react.PropTypes.node,
+    className: _react.PropTypes.string
   },
 
-  getDefaultProps () {
+  getDefaultProps: function getDefaultProps() {
     return {
       className: ''
-    }
-  },
-
-  render () {
-    const { children, className } = this.props;
-    const props = {
-      ...this.props,
-      className: `dropdown__content ${className}`
     };
+  },
+  render: function render() {
+    var _props = this.props;
+    var children = _props.children;
+    var className = _props.className;
 
-    return (
-      <div {...props}>
-        {children}
-      </div>
-    )
+    var props = _extends({}, this.props, {
+      className: 'dropdown__content ' + className
+    });
+
+    return _react2.default.createElement(
+      'div',
+      props,
+      children
+    );
   }
 });
 
-export default DropdownContent;
+exports.default = DropdownContent;
