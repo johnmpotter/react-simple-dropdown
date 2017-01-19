8f36ace74cf9fdca6d2754413508220bc2b49bab
John Potter <johnpotter1440@gmail.com>
2017-01-18 19:04:32 -0800

    Create NPM ready files

--- lib/components/Dropdown.jsx
+++ lib/components/Dropdown.jsx
@@ -1,103 +1,120 @@
-import React, { cloneElement, createClass } from 'react';
-import { findDOMNode } from 'react-dom';
-import cx from 'classnames';
+'use strict';
 
-import DropdownTrigger from './DropdownTrigger.js';
-import DropdownContent from './DropdownContent.js';
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DropdownContent = exports.DropdownTrigger = undefined;
 
-var Dropdown = createClass({
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+var _DropdownTrigger = require('./DropdownTrigger.js');
+
+var _DropdownTrigger2 = _interopRequireDefault(_DropdownTrigger);
+
+var _DropdownContent = require('./DropdownContent.js');
+
+var _DropdownContent2 = _interopRequireDefault(_DropdownContent);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Dropdown = (0, _react.createClass)({
   displayName: 'Dropdown',
 
-  getInitialState () {
+  getInitialState: function getInitialState() {
     return {
       active: false
     };
   },
-
-  getDefaultProps () {
+  getDefaultProps: function getDefaultProps() {
     return {
       className: ''
-    }
+    };
   },
-
-  componentDidMount () {
-    window.addEventListener( 'click', this._onWindowClick );
-    window.addEventListener( 'touchstart', this._onWindowClick );
+  componentDidMount: function componentDidMount() {
+    window.addEventListener('click', this._onWindowClick);
+    window.addEventListener('touchstart', this._onWindowClick);
   },
-
-  componentWillUnmount () {
-    window.removeEventListener( 'click', this._onWindowClick );
-    window.removeEventListener( 'touchstart', this._onWindowClick );
+  componentWillUnmount: function componentWillUnmount() {
+    window.removeEventListener('click', this._onWindowClick);
+    window.removeEventListener('touchstart', this._onWindowClick);
   },
+  render: function render() {
+    var _this = this,
+        _arguments = arguments;
 
-  render () {
-    const { children, className } = this.props;
+    var _props = this.props;
+    var children = _props.children;
+    var className = _props.className;
     // create component classes
-    const active = this.isActive();
-    var dropdown_classes = cx({
+
+    var active = this.isActive();
+    var dropdown_classes = (0, _classnames2.default)({
       dropdown: true,
       'dropdown--active': active
     });
     dropdown_classes += ' ' + className;
     // stick callback on trigger element
-    const bound_children = React.Children.map( children, child => {
-      if( child.type === DropdownTrigger ){
-        const originalOnClick = child.props.onClick;
-        child = cloneElement( child, {
-          ref: 'trigger',
-          onClick: ( event ) => {
-            this._onToggleClick( event );
-            if( originalOnClick ){
-              originalOnClick.apply( child, arguments );
+    var bound_children = _react2.default.Children.map(children, function (child) {
+      if (child.type === _DropdownTrigger2.default) {
+        (function () {
+          var originalOnClick = child.props.onClick;
+          child = (0, _react.cloneElement)(child, {
+            ref: 'trigger',
+            onClick: function onClick(event) {
+              _this._onToggleClick(event);
+              if (originalOnClick) {
+                originalOnClick.apply(child, _arguments);
+              }
             }
-          }
-        });
+          });
+        })();
       }
       return child;
     });
-    return (
-      <div
-        style={this.props.style}
-        className={dropdown_classes}>
-        {bound_children}
-      </div>
+    return _react2.default.createElement(
+      'div',
+      {
+        style: this.props.style,
+        className: dropdown_classes },
+      bound_children
     );
   },
-
-  isActive () {
-    return ( typeof this.props.active === 'boolean' ) ?
-      this.props.active :
-      this.state.active;
+  isActive: function isActive() {
+    return typeof this.props.active === 'boolean' ? this.props.active : this.state.active;
   },
-
-  hide () {
+  hide: function hide() {
     this.setState({
       active: false
     });
-    if( this.props.onHide ){
+    if (this.props.onHide) {
       this.props.onHide();
     }
   },
-
-  show () {
+  show: function show() {
     this.setState({
       active: true
     });
-    if( this.props.onShow ){
+    if (this.props.onShow) {
       this.props.onShow();
     }
   },
-
-  _onWindowClick ( event ) {
-    const dropdown_element = findDOMNode( this );
-    if( event.target !== dropdown_element && !dropdown_element.contains( event.target ) && this.isActive() ){
+  _onWindowClick: function _onWindowClick(event) {
+    var dropdown_element = (0, _reactDom.findDOMNode)(this);
+    if (event.target !== dropdown_element && !dropdown_element.contains(event.target) && this.isActive()) {
       this.hide();
     }
   },
-
-  _onToggleClick ( event ) {
+  _onToggleClick: function _onToggleClick(event) {
     event.preventDefault();
-    if( this.isActive() ){
+    if (this.isActive()) {
       this.hide();
     } else {
       this.show();
@@ -105,5 +122,6 @@ var Dropdown = createClass({
   }
 });
 
-export { DropdownTrigger, DropdownContent };
-export default Dropdown;
+exports.DropdownTrigger = _DropdownTrigger2.default;
+exports.DropdownContent = _DropdownContent2.default;
+exports.default = Dropdown;
