18c53261d37928ef65d3af772f0f8328c80c64d0
John Potter <johnpotter1440@gmail.com>
2017-01-18 19:06:21 -0800

    create NPM ready files

--- lib/components/DropdownTrigger.jsx
+++ lib/components/DropdownTrigger.jsx
@@ -1,32 +1,46 @@
-import React, { createClass, PropTypes } from 'react';
 
-const DropdownTrigger = createClass({
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var DropdownTrigger = (0, _react.createClass)({
   displayName: 'DropdownTrigger',
 
   propTypes: {
-    children: PropTypes.node,
-    className: PropTypes.string
+    children: _react.PropTypes.node,
+    className: _react.PropTypes.string
   },
 
-  getDefaultProps () {
+  getDefaultProps: function getDefaultProps() {
     return {
       className: ''
     };
   },
+  render: function render() {
+    var _props = this.props;
+    var children = _props.children;
+    var className = _props.className;
 
-  render () {
-    const { children, className } = this.props;
-    const props = {
-      ...this.props,
-      className: `dropdown__trigger ${className}`
-    };
+    var props = _extends({}, this.props, {
+      className: 'dropdown__trigger ' + className
+    });
 
-    return (
-      <div {...props} href="#dropdown-trigger">
-        {children}
-      </div>
+    return _react2.default.createElement(
+      'div',
+      _extends({}, props, { }),
+      children
     );
   }
 });
 
-export default DropdownTrigger;
+exports.default = DropdownTrigger;
